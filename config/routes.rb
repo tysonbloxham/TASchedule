e2a1db5d2bf6df7306442a31eb2dd6471d3b2e8f
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-08-11 16:22:42 -0600

    Added stylings and integrated the calendars and index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :requests
- root to: "requests#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/redirect', to: 'requests#redirect', as: 'redirect'
 	get '/callback', to: 'requests#callback', as: 'callback'
 	get '/calendars', to: 'requests#calendars', as: 'calendars'
 	get '/events/:calendar_id', to: 'requests#events', as: 'events', calendar_id: /[^\/]+/
 
-  root to: 'requests#index'
+  root to: 'requests#redirect'
 end
