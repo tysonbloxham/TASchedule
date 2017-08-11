2a7dbb432aa51fdaf7850d01d90729b45dd8d544
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-08-11 15:38:54 -0600

    Isolated date time from google calendar

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,10 +274,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  require ‘omniauth-google-oauth2’
-    config.omniauth :google_oauth2, ENV[‘GOOGLE_CLIENT_ID’],
-    ENV[‘GOOGLE_CLIENT_SECRET’],
-    { access_type: “offline”, approval_prompt: “”,
-    scope: ‘userinfo.email,calendar’ }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   
   resources :requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/redirect', to: 'requests#redirect', as: 'redirect'
+	get '/callback', to: 'requests#callback', as: 'callback'
+	get '/calendars', to: 'requests#calendars', as: 'calendars'
+	get '/events/:calendar_id', to: 'requests#events', as: 'events', calendar_id: /[^\/]+/
 
   root to: 'requests#index'
 end
